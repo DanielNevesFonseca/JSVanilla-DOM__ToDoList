6dac3e8a7dfcd319a5f45340c8097387b737deac
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2023-05-11 13:38:26 -0300

    código refatorado e troca de métodos de arrays por função criada manualmente.

--- script.js
+++ script.js
@@ -1,94 +1,86 @@
 const tasks = [
   {
-    titulo: "Comprar comida para o gato",
-    tipo: "Urgente",
+    title: "Comprar comida para o gato",
+    type: "Urgente",
   },
   {
-    titulo: "Consertar Computador",
-    tipo: "Prioritário",
+    title: "Consertar Computador",
+    type: "Prioritário",
   },
   {
-    titulo: "Beber água",
-    tipo: "Normal",
+    title: "Beber água",
+    type: "Normal",
   },
 ];
 
 function createCard(taskInfo) {
-  // Criando elementos necessários
-  const li = document.createElement("li");
-  const div = document.createElement("div");
-  const span = document.createElement("span");
-  const p = document.createElement("p");
+  const item = document.createElement("li");
+  const infoContainer = document.createElement("div");
+  const taskCategory = document.createElement("span");
+  const taskTitle = document.createElement("p");
 
-  // Adicionando o titulo da tarefa como texto do paragrafo
-  p.innerText = taskInfo.titulo;
+  taskTitle.innerText = taskInfo.title;
+  let titleTask = taskTitle.innerText;
 
-  if (taskInfo.tipo === "Urgente") {
-    span.className = 'span-urgent';
-  } else if (taskInfo.tipo === "Prioritário") {
-    span.className = 'span-priority';
-  } else if (taskInfo.tipo === "Normal") {
-    span.className = 'span-normal';
+  if (taskInfo.type === "Urgente") {
+    taskCategory.className = 'span-urgent';
+  } else if (taskInfo.type === "Prioritário") {
+    taskCategory.className = 'span-priority';
+  } else if (taskInfo.type === "Normal") {
+    taskCategory.className = 'span-normal';
   }
+  infoContainer.appendChild(taskCategory);
+  infoContainer.appendChild(taskTitle);
 
-  // Adicionando span e paragrafo a div
-  div.appendChild(span);
-  div.appendChild(p);
-
-  // Criando botão para deletar tarefa
   const button = document.createElement("button");
 
-  // Adicionando icone ao botão
   button.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
   button.addEventListener("click", function () {
-    // Encontrando o índice da tarefa no array tasks por meio de uma callback
-    const index = tasks.findIndex(task => task.titulo === taskInfo.titulo
-      && task.tipo === taskInfo.tipo);
-    console.log(index)
+    function manualIndexOf(array, element) {
+      for (let i = 0; i < array.length; i++) {
+        if (array[i].title === element) {
+          return i;
+        }
+      }
+      return -1;
+    }
+    const index = manualIndexOf(tasks, titleTask);
+    console.log(index, titleTask)
     tasks.splice(index, 1);
-    renderElements(tasks);
+    renderElements(tasks); 
+  });
+  item.appendChild(infoContainer);
+  item.appendChild(button);
 
-  })
-
-
-  /// Adicionando a div e o botão de deletar ao list item
-  li.appendChild(div);
-  li.appendChild(button);
-
-  return li;
+  return item;
 }
 
 function renderElements(taskList) {
   const htmlList = document.querySelector(".tasks");
   htmlList.innerHTML = "";
 
-  // Ajustar a lógica
   for (let i = 0; i < taskList.length; i++) {
     let card = createCard(taskList[i]);
     htmlList.appendChild(card);
   }
-
   return htmlList;
-
 }
 renderElements(tasks);
 
 function createNewTask(taskList) {
   const taskTitle = document.querySelector('#input_title');
-  const taskType = document.querySelector('#input_priority');
+  const taskCategory = document.querySelector('#input_priority');
   const submitBtn = document.querySelector('#btnSubmit');
 
   submitBtn.addEventListener('click', function (event) {
     // evitar que o efeito bubbling faça sair da página atual
     event.preventDefault();
     const newTaskInfo = {
-      titulo: taskTitle.value,
-      tipo: taskType.value
+      title: taskTitle.value,
+      type: taskCategory.value
     }
     taskList.push(newTaskInfo);
     renderElements(taskList);
   })
 }
 createNewTask(tasks);
-
-
